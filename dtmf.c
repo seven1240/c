4d3840f58b3437a16ba44ce590948fa8299578de
Seven Du <dujinfang@x-y-t.cn>
2020-11-06 13:00:03 +0800

    update

--- dtmf.c
+++ dtmf.c
@@ -17,18 +17,23 @@ int main(int argc, char **argv)
 
     fd = open(argv[1], O_RDONLY);
 
-    if (fd < 0) return 1;
+    if (fd < 0) {
+        printf("error\n");
+        return 1;
+    }
 
     while (1) {
-        len = read(fd, data, sizeof(data));
+        len = read(fd, data, 320);
 
         if (len <=0) break;
 
-        if (teletone_dtmf_detect(&dtmf_detect, (short *)data, len / 2) == TT_HIT_END) {
-            teletone_dtmf_get(&dtmf_detect, &digit, &duration);
-            printf("%c ", digit);
-        }
+        teletone_hit_type_t ret = teletone_dtmf_detect(&dtmf_detect, (short *)data, len / 2);
+        printf("ret = %d\n", ret);
 
+        if (ret == TT_HIT_END) {
+            teletone_dtmf_get(&dtmf_detect, &digit, &duration);
+            printf("DTMF = %c\n", digit);
+        }
     }
 
     close(fd);
